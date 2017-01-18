80512d1ebc9d6abb8fe5f2170da5dd3e57578b10
Ryan Clark <Rymaster95@gmail.com>
2017-01-18 15:18:45 -0700

    completed afternoon project day 3 of angular

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,21 +2,68 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+$scope.getMessages = function() {
+  messageService.getMessages().then(function(response) {
+//     var messages = response.data;
+// for (var i = 0; i < messages.length; i++){
+//   messages[i].createdAt = moment().format('YYYY MM DD, h:mm:ss a')
+// }
+$scope.messages = response.data;
+  })
+}
 
+$scope.getCookies = function() {
+  messageService.getCookies().then(function(response) {
 
+$scope.cookies = response.data;
+  })
+}
 
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
-
+$scope.postMessage = function(message) {
+  messageService.postMessage(message).then(function(response){
+    response;
+    $scope.message = '';
+  })
+}
 
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
+   setInterval(function(){
+    $scope.getMessages();
 
-  // }, 1500)
+
+  }, 1500)
+
+  $scope.sortDirection = '-'
+  $scope.upDowns = [
+    {
+      name:'Ascending',
+      Value:'+'
+    },
+    {
+      name:'Descending',
+      Value:'-'
+    }
+  ];
+  $scope.sortProp = 'createdAt';
+$scope.sorts = [
+  {
+    name:'Message',
+    Value:'message'
+  },
+  {
+    name:'Time',
+    Value:'createdAt'
+  },
+  {
+    name:'Cookie',
+    Value:'cookie'
+  }
+];
 
 })
